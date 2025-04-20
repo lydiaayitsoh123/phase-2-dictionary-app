38fc61a434e1e1dc4222045e3b89c1e6055f782e
lydiaayitsoh123 <lydiaayitsoh@gmail.com>
2025-04-20 13:09:04 +0300

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ const SearchBar = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
 
   const handleChange = (e) => {
-    setSearchTerm(e.target.);
+    setSearchTerm(e.target.value);
   };
 
   const handleSubmit = (e) => {
